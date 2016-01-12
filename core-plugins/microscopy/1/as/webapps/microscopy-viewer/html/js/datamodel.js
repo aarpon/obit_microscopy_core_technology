91b8b7745710fcf79fca2dbd76b00baae5176553
Aaron Ponti <aarpon@gmail.com>
2016-01-12 17:06:01 +0100

    The microscopy-viewer web app now uses the new copy_microscopy_datasets_to_userdir server-side plug-in as well.

--- core-plugins/microscopy/1/as/webapps/microscopy-viewer/html/js/datamodel.js
+++ core-plugins/microscopy/1/as/webapps/microscopy-viewer/html/js/datamodel.js
@@ -229,124 +229,6 @@ DataModel.prototype.getDataSetsForSampleAndExperiment = function(action) {
 
 }
 
-/**
- * Return the series name for the dataset with given code.
- * @param datasetCode Code of the series dataset
- * @returns name of the series.
- */
-DataModel.prototype.getSeriesName = function(datasetCode) {
-    // TODO: Return the series name
-    return datasetCode;
-}
-
-/**
- * Call an aggregation plug-in to copy the datasets associated to the experiment.
- * @param {type} ?
- * @param {type} ?
- * @param {type} ?
- * @returns {tubes} ?
- */
-DataModel.prototype.copyDatasetsToUserDir = function(experimentId, mode) {
-
-    // Add call to the aggregation service
-    var parameters = {
-        experimentId: experimentId,
-        mode: mode
-    };
-
-    // Get the datastore server name from the configuration
-    var dataStoreServer = CONFIG['dataStoreServer'];
-
-    // Inform the user that we are about to process the request
-    DATAVIEWER.displayStatus("Please wait while processing your request. This might take a while...", "info");
-
-    // Must use global object
-    DATAMODEL.openbisServer.createReportFromAggregationService(dataStoreServer,
-        "copy_microscopy_datasets_to_userdir", parameters, function(response) {
-
-            var status;
-            var unexpected = "Sorry, unexpected feedback from server " +
-                "obtained. Please contact your administrator.";
-            var level = "";
-            var row;
-
-            // Returned parameters
-            var r_Success;
-            var r_ErrorMessage;
-            var r_NCopiedFiles;
-            var r_RelativeExpFolder;
-            var r_ZipArchiveFileName;
-            var r_Mode;
-
-            if (response.error) {
-                status = "Sorry, could not process request.";
-                level = "error";
-                r_Success = false;
-            } else {
-                status = "";
-                if (response.result.rows.length != 1) {
-                    status = unexpected;
-                    level = "error";
-                } else {
-                    row = response.result.rows[0];
-                    if (row.length != 6) {
-                        status = unexpected;
-                        level = "error";
-                    } else {
-
-                        // Extract returned values for clarity
-                        r_Success = row[0].value;
-                        r_ErrorMessage = row[1].value;
-                        r_NCopiedFiles = row[2].value;
-                        r_RelativeExpFolder = row[3].value;
-                        r_ZipArchiveFileName = row[4].value;
-                        r_Mode = row[5].value;
-
-                        if (r_Success == true) {
-                            var snip = "<b>Congratulations!</b>&nbsp;";
-                            if (r_NCopiedFiles == 1) {
-                                snip = snip +
-                                    "<span class=\"badge\">1</span> file was ";
-                            } else {
-                                snip = snip +
-                                    "<span class=\"badge\">" +
-                                    r_NCopiedFiles + "</span> files were ";
-                            }
-                            if (r_Mode == "normal") {
-                                status = snip + "successfully exported to " +
-                                    "{...}/" + r_RelativeExpFolder + ".";
-                            } else {
-                                // Add a placeholder to store the download URL.
-                                status = snip + "successfully packaged. <span id=\"download_url_span\"></span>";
-                            }
-                            level = "success";
-                        } else {
-                            if (r_Mode == "normal") {
-                                status = "Sorry, there was an error exporting " +
-                                    "to your user folder:<br /><br />\"" +
-                                    r_ErrorMessage + "\".";
-                            } else {
-                                status = "Sorry, there was an error packaging your files for download!";
-                            }
-                            level = "error";
-                        }
-                    }
-                }
-            }
-            DATAVIEWER.displayStatus(status, level);
-
-            // Retrieve the URL (asynchronously)
-            if (r_Success == true && r_Mode == "zip")
-                DATAMODEL.openbisServer.createSessionWorkspaceDownloadUrl(r_ZipArchiveFileName,
-                    function(url) {
-                        var downloadString =
-                            '<img src="img/download.png" />&nbsp;<a href="' + url + '">Download</a>!';
-                        //'<a href="' + url + '"><img src = "img/download.png" />&nbsp;Download</a>';
-                        $("#download_url_span").html(downloadString);
-                    });
-        });
-
-};
 
 /**
  * Call an aggregation plug-in to copy the datasets associated to the experiment and sample.
@@ -364,98 +246,150 @@ DataModel.prototype.copyDatasetsToUserDir = function(experimentId, sampleId, mod
         mode: mode
     };
 
-    // Get the datastore server name from the configuration
-    var dataStoreServer = CONFIG['dataStoreServer'];
-
     // Inform the user that we are about to process the request
     DATAVIEWER.displayStatus("Please wait while processing your request. This might take a while...", "info");
 
     // Must use global object
-    DATAMODEL.openbisServer.createReportFromAggregationService(dataStoreServer,
-        "copy_microscopy_datasets_to_userdir", parameters, function(response) {
+    DATAMODEL.openbisServer.createReportFromAggregationService(CONFIG['dataStoreServer'],
+        "copy_microscopy_datasets_to_userdir", parameters,
+        DATAMODEL.processResultsFromCopyDatasetsToUserDirServerSidePlugin );
+};
 
-            var status;
-            var unexpected = "Sorry, unexpected feedback from server " +
-                "obtained. Please contact your administrator.";
-            var level = "";
-            var row;
+/**
+ * Process the results returned from the copyDatasetsToUserDir() server-side plug-in
+ * @param response JSON object
+ */
+DataModel.prototype.processResultsFromCopyDatasetsToUserDirServerSidePlugin = function(response) {
 
-            // Returned parameters
-            var r_Success;
-            var r_ErrorMessage;
-            var r_NCopiedFiles;
-            var r_RelativeExpFolder;
-            var r_ZipArchiveFileName;
-            var r_Mode;
+    var status;
+    var unexpected = "Sorry, unexpected feedback from server " +
+        "obtained. Please contact your administrator.";
+    var level = "";
+    var row;
 
-            if (response.error) {
-                status = "Sorry, could not process request.";
-                level = "error";
-                r_Success = false;
+    // Returned parameters
+    var r_UID;
+    var r_Completed;
+    var r_Success;
+    var r_ErrorMessage;
+    var r_NCopiedFiles;
+    var r_RelativeExpFolder;
+    var r_ZipArchiveFileName;
+    var r_Mode;
+
+    // First check if we have an error
+    if (response.error) {
+
+        // Indeed there was an error.
+        status = "Sorry, could not process request.";
+        level = "error";
+        r_Success = false;
+
+    } else {
+
+        // No obvious error. Retrieve the results
+        status = "";
+        if (response.result.rows.length != 1) {
+
+            // Unexpected number of rows returned
+            status = unexpected;
+            level = "error";
+
+        } else {
+
+            // We have a (potentially) valid result
+            row = response.result.rows[0];
+
+            // Retrieve the uid
+            r_UID = row[0].value;
+
+            // Retrieve the 'completed' status
+            r_Completed = row[1].value;
+
+            // If the processing is not completed, we wait a few seconds and trigger the
+            // server-side plug-in again. The interval is defined by the admin.
+            if (r_Completed == false) {
+
+                // We only need the UID of the job
+                parameters = {};
+                parameters["uid"] = r_UID;
+
+                // Call the plug-in
+                setTimeout(function() {
+                        DATAMODEL.openbisServer.createReportFromAggregationService(
+                            CONFIG['dataStoreServer'],
+                            "copy_microscopy_datasets_to_userdir", parameters,
+                            DATAMODEL.processResultsFromCopyDatasetsToUserDirServerSidePlugin)
+                    },
+                    parseInt(CONFIG['queryPluginStatusInterval']));
+                // Return here
+                return;
+
             } else {
-                status = "";
-                if (response.result.rows.length != 1) {
+
+                // First, check if the process is finished or whether it is still running
+
+                if (row.length != 8) {
+
+                    // Again, something is wrong with the returned results
                     status = unexpected;
                     level = "error";
+
                 } else {
-                    row = response.result.rows[0];
-                    if (row.length != 6) {
-                        status = unexpected;
-                        level = "error";
-                    } else {
 
-                        // Extract returned values for clarity
-                        r_Success = row[0].value;
-                        r_ErrorMessage = row[1].value;
-                        r_NCopiedFiles = row[2].value;
-                        r_RelativeExpFolder = row[3].value;
-                        r_ZipArchiveFileName = row[4].value;
-                        r_Mode = row[5].value;
+                    // Extract returned values for clarity
+                    r_Success = row[2].value;
+                    r_ErrorMessage = row[3].value;
+                    r_NCopiedFiles = row[4].value;
+                    r_RelativeExpFolder = row[5].value;
+                    r_ZipArchiveFileName = row[6].value;
+                    r_Mode = row[7].value;
 
-                        if (r_Success == true) {
-                            var snip = "<b>Congratulations!</b>&nbsp;";
-                            if (r_NCopiedFiles == 1) {
-                                snip = snip +
-                                    "<span class=\"badge\">1</span> file was ";
-                            } else {
-                                snip = snip +
-                                    "<span class=\"badge\">" +
-                                    r_NCopiedFiles + "</span> files were ";
-                            }
-                            if (r_Mode == "normal") {
-                                status = snip + "successfully exported to " +
-                                    "{...}/" + r_RelativeExpFolder + ".";
-                            } else if (r_Mode == "hrm") {
-                                status = snip + "successfully exported to your HRM source folder.";
-                            } else {
-                                // Add a placeholder to store the download URL.
-                                status = snip + "successfully packaged. <span id=\"download_url_span\"></span>";
-                            }
-                            level = "success";
+                    if (r_Success == true) {
+                        var snip = "<b>Congratulations!</b>&nbsp;";
+                        if (r_NCopiedFiles == 1) {
+                            snip = snip +
+                                "<span class=\"badge\">1</span> file was ";
                         } else {
-                            if (r_Mode == "normal") {
-                                status = "Sorry, there was an error exporting " +
-                                    "to your user folder:<br /><br />\"" +
-                                    r_ErrorMessage + "\".";
-                            } else {
-                                status = "Sorry, there was an error packaging your files for download!";
-                            }
-                            level = "error";
+                            snip = snip +
+                                "<span class=\"badge\">" +
+                                r_NCopiedFiles + "</span> files were ";
                         }
+                        if (r_Mode == "normal") {
+                            status = snip + "successfully exported to " +
+                                "{...}/" + r_RelativeExpFolder + ".";
+                        } else if (r_Mode == "hrm") {
+                            status = snip + "successfully exported to your HRM source folder.";
+                        } else {
+                            // Add a placeholder to store the download URL.
+                            status = snip + "successfully packaged. <span id=\"download_url_span\"></span>";
+                        }
+                        level = "success";
+                    } else {
+                        if (r_Mode == "normal") {
+                            status = "Sorry, there was an error exporting " +
+                                "to your user folder:<br /><br />\"" +
+                                r_ErrorMessage + "\".";
+                        } else {
+                            status = "Sorry, there was an error packaging your files for download!";
+                        }
+                        level = "error";
                     }
                 }
             }
-            DATAVIEWER.displayStatus(status, level);
+        }
+    }
+    DATAVIEWER.displayStatus(status, level);
 
-            // Retrieve the URL (asynchronously)
-            if (r_Success == true && r_Mode == "zip")
-                DATAMODEL.openbisServer.createSessionWorkspaceDownloadUrl(r_ZipArchiveFileName,
-                    function(url) {
-                        var downloadString =
-                            '<img src="img/download.png" />&nbsp;<a href="' + url + '">Download</a>!';
-                        //'<a href="' + url + '"><img src = "img/download.png" />&nbsp;Download</a>';
-                        $("#download_url_span").html(downloadString);
-                    });
-        });
+    // Retrieve the URL (asynchronously)
+    if (r_Success == true && r_Mode == "zip") {
+        DATAMODEL.openbisServer.createSessionWorkspaceDownloadUrl(r_ZipArchiveFileName,
+            function(url) {
+                var downloadString =
+                    '<img src="img/download.png" />&nbsp;<a href="' + url + '">Download</a>!';
+                //'<a href="' + url + '"><img src = "img/download.png" />&nbsp;Download</a>';
+                $("#download_url_span").html(downloadString);
+            });
+    }
 
 };
